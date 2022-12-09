8b89d5246726340ba81d37fc90d52424c875e529
NicklasVraa <n.k.vraa@gmail.com>
2022-12-09 19:55:53 +0100

    cleanup

--- notebooks/models.py
+++ notebooks/models.py
@@ -1,10 +1,11 @@
 from torch import nn
 import torch
 
+# Blocks components. ---------------------------------------------------
 class Conv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
         super(Conv, self).__init__()
-        
+
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.BatchNorm2d(out_channels),
@@ -13,11 +14,11 @@ class Conv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-    
+
 class ConvTranspose(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
         super(ConvTranspose, self).__init__()
-        
+
         self.conv = nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.BatchNorm2d(out_channels),
@@ -27,10 +28,11 @@ class ConvTranspose(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+# Full models. ---------------------------------------------------------
 class VAEModel(nn.Module):
     def __init__(self, base):
         super(VAEModel, self).__init__()
-        
+
         self.encoder = nn.Sequential(
             Conv(1, base, 3, stride=2, padding=1),
             Conv(base, 2*base, 3, padding=1),
@@ -46,7 +48,7 @@ class VAEModel(nn.Module):
         )
         self.encoder_mu = nn.Conv2d(64*base, 32*base, 1)
         self.encoder_logvar = nn.Conv2d(64*base, 32*base, 1)
-        
+
         self.decoder = nn.Sequential(
             nn.Conv2d(32*base, 64*base, 1),
             ConvTranspose(64*base, 4*base, 8),
@@ -63,7 +65,7 @@ class VAEModel(nn.Module):
             nn.Conv2d(base, 1, 3, padding=1),
             nn.Sigmoid()
         )
-        
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
@@ -73,7 +75,7 @@ class VAEModel(nn.Module):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
-        
+
     def encode(self, x):
         x = self.encoder(x)
         return self.encoder_mu(x), self.encoder_logvar(x)
@@ -82,7 +84,7 @@ class VAEModel(nn.Module):
         std = torch.exp(0.5*logvar)
         eps = torch.randn_like(std)
         return mu + eps*std
-    
+
     def decode(self, z):
         return self.decoder(z)
 
@@ -99,7 +101,7 @@ class VAEModel(nn.Module):
 class SegmentationModel(nn.Module):
     def __init__(self, base):
         super(SegmentationModel, self).__init__()
-        
+
         self.decoder = nn.Sequential(
             nn.Conv2d(32*base, 64*base, 1),
             ConvTranspose(64*base, 4*base, 8),
@@ -116,7 +118,7 @@ class SegmentationModel(nn.Module):
             nn.Conv2d(base, 1, 3, padding=1),
             nn.Sigmoid()
         )
-        
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
